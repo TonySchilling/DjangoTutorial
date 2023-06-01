803decbc59421d009bee8aff4823d4d6710e3291
TonySchilling <tjjpschilling7@gmail.com>
2023-06-01 14:20:37 -0500

    Add login

--- myApp/views.py
+++ myApp/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password =request.POST['password']
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Issue loggin in")
+            return redirect('home')
+
     return render(request, 'home.html', {})
 
 
@@ -12,5 +27,9 @@ def login_user(request):
     pass
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You Have Been Logged Out..")
+    return redirect('home')
 
+def register_user(request):
+    return render(request, 'register.html', {})
